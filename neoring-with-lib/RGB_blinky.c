3c4ee68b941934b81ace6eaa4f184b844975120b
Alejandro Gallo <aamsgallo@gmail.com>
2019-02-07 01:23:20 +0100

    Update ring

--- neoring-with-lib/RGB_blinky.c
+++ neoring-with-lib/RGB_blinky.c
@@ -6,42 +6,87 @@
 #define ws2812_pin  2
 #define LED_LENGTH 16
 #define ws2812_resettime  50
+#define DELAY_1 8000
+#define LAPS 10
 
 #include "lib/light_ws2812.h"
+
+void clear(struct cRGB *led, int length) {
+  uint8_t i;
+  for (i = 0; i < length; ++i) {
+    led[i].r=0;
+    led[i].g=0;
+    led[i].b=0;
+  }
+  ws2812_setleds(led,length);
+}
 
 struct cRGB led[LED_LENGTH];
 
 int main(void)
 {
-  #ifdef __AVR_ATtiny10__
-  CCP=0xD8;		// configuration change protection, write signature
-  CLKPSR=0;		// set cpu clock prescaler =1 (8Mhz) (attiny 4/5/9/10)
-  #endif
-  int i;
-  const int colors[3][3] = {{123, 0, 1}, {244, 12, 10}, {123, 123, 0}};
+  uint8_t i, lap;
+  const uint8_t colors[3][3] = {
+    {20, 128, 21},
+    {244, 12, 10},
+    {123, 123, 0}
+  };
 
-  for (i = 0; i < LED_LENGTH; ++i) {
-    led[i].r=0;led[i].g=00;led[i].b=0;    // Write red to array
-    ws2812_setleds(led,i+1);
-    _delay_ms(1);                         // wait for 500ms.
-  }
+  clear(led, LED_LENGTH);
+  _delay_ms(1e3);
 
   while(1)
   {
-    led[0].r=255;led[0].g=100;led[0].b=0;    // Write red to array
-    ws2812_setleds(led,1);
-    _delay_ms(100);                         // wait for 500ms.
-    for (i = 1; i < LED_LENGTH; ++i) {
 
-      led[i-1].r=0;led[i-1].g=0;led[i-1].b=0;    // Write red to array
-      led[i].r=colors[i%3][0];led[i].g=colors[i%3][1];led[i].b=colors[i%3][2];    // Write red to array
-      ws2812_setleds(led,i);
-      ws2812_setleds(led,i+1);
-      _delay_ms(100);                         // wait for 500ms.
+    for (lap = 0; lap < LAPS; ++lap) {
+
+      led[0].r=255;led[0].g=100;led[0].b=0;
+      ws2812_setleds(led,1);
+      _delay_us(DELAY_1);
+      for (i = 1; i < LED_LENGTH; ++i) {
+
+        led[i-1].r=0;
+        led[i-1].g=0;
+        led[i-1].b=0;
+        led[i].r=colors[i%3][0];
+        led[i].g=colors[i%3][1];
+        led[i].b=colors[i%3][2];
+        ws2812_setleds(led,16);
+        _delay_us(DELAY_1);
+
+      }
+      led[15].r=0;led[15].g=0;led[15].b=0;
+      ws2812_setleds(led,16);
+      _delay_us(DELAY_1);
 
     }
-    led[15].r=0;led[15].g=0;led[15].b=0;    // Write red to array
-    ws2812_setleds(led,16);
-    _delay_ms(100);                         // wait for 500ms.
+
+    clear(led, LED_LENGTH);
+
+    for (lap = 0; lap < 255; ++lap) {
+
+
+      for (i = 0; i < LED_LENGTH+1; ++i) {
+
+        led[i].r = (2*lap) % 255;
+        led[i].g = (255 - (1*lap)%255);
+        led[i].b = 0;
+
+        /*led[i].r = led[i-1].r;*/
+        /*led[i].g = led[i-1].g;*/
+        /*led[i].b = led[i-1].b;*/
+
+        led[i-1].r = 0;
+        led[i-1].g = 0;
+        led[i-1].b = 0;
+
+        ws2812_setleds(led,16);
+        _delay_us(1);
+
+      }
+
+
+    }
+
   }
 }
--- neoring-with-lib/config.mk
+++ neoring-with-lib/config.mk
@@ -3,8 +3,8 @@ MCU = attiny85
 MCU = attiny45
 
 
-F_CPU = 1200000UL
 F_CPU = 9600000UL
+F_CPU = 1200000UL
 F_CPU = 8000000
 
 CFLAGS += -Ilib/ -Llib/
