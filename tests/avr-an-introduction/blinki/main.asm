358226159736932500ff6dc2d7e55a9d9d594a33
Alejandro Gallo <aamsgallo@gmail.com>
2019-10-06 03:32:57 +0200

    Working blinky with timer

--- tests/avr-an-introduction/blinki/main.asm
+++ tests/avr-an-introduction/blinki/main.asm
@@ -1,46 +1,79 @@
 #define __SFR_OFFSET 0
 .nolist
 #include <avr/io.h>
+#include <avr/interrupt.h>
 .list
 
-#define temp r16
-
-.global setup
-setup:
-in r16, TCCR0
-ldi r16, 1
-out TCCR0A, r16
-rjmp main
+.global TIM0_COMPA_vect
+TIM0_COMPA_vect:
+  in r18, PORTB
+  ldi r19, 0b0000001
+  eor r18, r19
+  out PORTB, r18
+  reti
 
 .global main
 main:
-sei
-ldi r16, 0b11111
-in r17, DDRB
-or r17, r16
-out DDRB, r17
+  ser r16
+  out DDRB, r16
+  clr r16
+  out PORTB, r16
 
-in r17, PORTB
-or r17, r16
-out PORTB, r17
-rjmp light
+  in r16, TCCR0A
+  ori r16, _BV(WGM01)
+  out TCCR0A, r16 ; set system clock prescaler for counter
 
-.global light
+  in r16, TCCR0B
+  ori r16, 0b00000101
+  out TCCR0B, r16 ; set system clock prescaler for counter
+
+  ;ser r16
+  ;out OCR0B, r16
+
+  ldi r16, 0xff
+  out OCR0A, r16
+
+  in r16, TIMSK0
+  ori r16, (1<<OCIE0A)
+  ;ori r16, (1<<OCIE0B)
+  out TIMSK0, r16
+
+  sei
+
+
+  rjmp .-2
+
+;.global main
+;main:
+  ;rjmp init
+
+;loop:
+  ;in r16, TCNT0
+  ;out PORTB, r16
+  ;in r16, TCNT0
+  ;cpi r16, 0xff
+  ;breq light
+  ;rjmp loop
+
+;;ldi r16, 0xff
+;;out TCNT0, r16
+
+;rjmp main
+
+;.global main
+;main:
+;in r16, TCNT0
+;cpi r16, 0xff
+;breq light
+
 light:
-push r16
-push r17
-in r16, PORTB
-ldi r17, 1
-add r16, r17
-out PORTB, r16
-pop r17
-pop r16
-rjmp main
 
-TIM0_COMPA:
-in r16, PORTB
-andi r16, 0x00
+light2:
 
-TIM0_COMPB:
-in r16, PORTB
-andi r16, 0x00
+;TIM0_COMPB_vect:
+  ;in r16, PORTB
+  ;ldi r17, 1
+  ;eor r16, r17
+  ;out PORTB, r16
+  ;rjmp loop
+  ;reti
