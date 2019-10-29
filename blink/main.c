fef2b39cb012e35405ae62ef2cf37604668fe5b4
Alejandro Gallo <aamsgallo@gmail.com>
2019-10-29 23:46:45 +0100

    Clean up main.c

--- blink/main.c
+++ blink/main.c
@@ -5,18 +5,28 @@
 #define RED 1<<0
 #define GREEN 1<<1
 
-#define DELAY 100
+#define DELAY 1000
+#define led PORTB0
 
 int
 main(void) {
 
+  DDRB = 0b111111;
+
   while (1) {
+    PORTB = (1<<0);
     _delay_ms(DELAY);
-    PORTB ^= BLUE;
+    PORTB = (1<<1);
     _delay_ms(DELAY);
-    PORTB ^= RED;
+    PORTB = (1<<2);
     _delay_ms(DELAY);
-    PORTB ^= GREEN;
+    PORTB = (1<<3);
+    _delay_ms(DELAY);
+    PORTB = (1<<4);
+    _delay_ms(DELAY);
+    PORTB = (1<<5);
+    _delay_ms(DELAY);
+    PORTB = 0;
   }
   return 0;
 }
--- ws2812/main.c
+++ ws2812/main.c
@@ -1,71 +1,13 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define WS2812_DDR DDRB
-#define WS2812_PORT PORTB
 #define WS2812_DIN_PIN PB0
-#define WS2812_INIT() WS2812_DDR |= (1<<WS2812_DIN_PIN)
-#define FAST
-#ifdef FAST
-#  define WS2812_DIN_HIGH() WS2812_PORT = 1
-#  define WS2812_DIN_LOW() WS2812_PORT = 0
-#else
-#  define WS2812_DIN_HIGH() WS2812_PORT |= (1<<WS2812_DIN_PIN)
-#  define WS2812_DIN_LOW() WS2812_PORT &= ~(1<<WS2812_DIN_PIN)
-#endif
+#include "ws2812.h"
 
-/*
-T0H 350 ns
-T0L 800 ns
-
-T1H 700 ns
-T1L 600 ns
-
-nop = 80ns
-*/
-
-struct Led {
-  uint8_t r;
-  uint8_t g;
-  uint8_t b;
-};
-
-void send_char(uint8_t c) {
-  uint8_t i;
-  for (i = 0; i < 8; i++, c<<=1) {
-    if ( c & 0x80 ) {
-      WS2812_DIN_HIGH();
-      __asm("nop");
-      __asm("nop");
-      WS2812_DIN_LOW();
-    } else {
-      WS2812_DIN_HIGH();
-      WS2812_DIN_LOW();
-      __asm("nop");
-      __asm("nop");
-    }
-  }
-}
-
-void send_led(struct Led l) {
-  send_char(l.g);
-  send_char(l.r);
-  send_char(l.b);
-  _delay_us(2);
-}
-
-void send_leds(struct Led ls[], uint8_t n) {
-  uint8_t i;
-  for (i = 0; i < n; i++) {
-    send_char(ls[i].g);
-    send_char(ls[i].r);
-    send_char(ls[i].b);
-  }
-  _delay_us(2);
-}
 
 int main() {
 
+  /*
 #define NLIGHTS 16
   struct Led l[NLIGHTS] = {
     {0xff, 0, 0},
@@ -85,6 +27,7 @@ int main() {
     {0, 0xff, 0},
     {0, 0, 0xff},
   };
+  */
 
   WS2812_INIT();
 
